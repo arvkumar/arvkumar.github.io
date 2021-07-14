35ad0bfd83778eb63e7d1d51d989b817a4142155
arvkumar <arvind.k.panchal@gmail.com>
2021-07-14 14:49:16 +0200

    update

--- video/video.component.js
+++ video/video.component.js
@@ -1,6 +1,18 @@
-// Register the `app-people` component on the `app-people` module,
+// Register the `publications` component on the `publications` module,
 angular.
 module('video').
 component('video', {
     templateUrl: 'video/video.template.html',
+    controller: ['$http',
+        function VideoController($http) {
+            var self = this;
+            self.orderProp = '-year';
+
+            $http.get('video/video.json').then(function(response) {
+                self.video = response.data;
+            });
+
+        }
+    ]
+
 });
